faf3e860bb06dba4b876ed74bde4507a19fa4d4a
Juan Rendon <juan.rendon@yuxiglobal.com>
2017-07-17 15:37:06 -0500

    Angular tour of heroes - Services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Hero } from './hero';
+
+import { HeroService } from './hero.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  title = 'Tour Of Heroes';
+  heroes: Hero[];
+  selectedHero : Hero;
+
+  constructor( private heroService: HeroService ){}
+
+  ngOnInit() {
+    this.getHeroes();
+  }
+
+  // getHeroes(): void {
+  //   this.heroService.getHeroes()
+  //       .then(heroes => this.heroes = heroes);
+  // }
+
+  //Simulate server latency with 2 seconds delay
+  getHeroes(): void {
+    this.heroService.getHeroesSlowly()
+        .then(heroes => this.heroes = heroes);
+  }
+  
+  onSelect(hero:Hero):void{ 
+    this.selectedHero = hero;
+  }
 }
