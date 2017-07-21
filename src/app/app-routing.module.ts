171730702599ecd2acb5ba5e255526215f3bfcd2
Juan Rendon <juan.rendon@yuxiglobal.com>
2017-07-21 15:54:40 -0500

    builda failed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,6 +9,7 @@ const routes: Routes = [
     { path: 'heroes', component: HeroesComponent },
     { path: 'dashboard',component: DashboardComponent },
     { path: 'detail/:id', component: HeroDetailComponent },
+    { path: '**', component: DashboardComponent },
     { path: '', redirectTo: '/dashboard', pathMatch: 'full' }
 ];
 
